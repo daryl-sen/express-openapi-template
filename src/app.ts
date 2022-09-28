8a3322ae7d438366456785bd492b9c88bdc91d1d
Daryl <tsdaryl@gmail.com>
2022-09-27 20:06:11 -0700

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
+import express, { NextFunction, Request as ExRequest, Response as ExResponse } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { ValidateError } from 'tsoa';
+
 // if routes don't exist, run `yarn run dev` first, refer to readme
-import { RegisterRoutes } from "../build/routes";
-import express, { Response as ExResponse, Request as ExRequest } from "express";
-import swaggerUi from "swagger-ui-express";
+import { RegisterRoutes } from '../build/routes';
 
 export const app = express();
 
@@ -25,3 +27,32 @@ app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
 app.use(bodyParser.json());
 
 RegisterRoutes(app);
+
+app.use(function notFoundHandler(_req, res: ExResponse) {
+  // if no matching endpoint
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
+
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -1,26 +1,34 @@
 import {
-  Body,
-  Controller,
-  Get,
-  Path,
-  Post,
-  Query,
-  Route,
-  SuccessResponse,
-} from "tsoa";
-import { User } from "./user";
-import { UsersService, UserCreationParams } from "./usersService";
+    Body, Controller, Get, Path, Post, Query, Res, Response, Route, SuccessResponse, TsoaResponse
+} from 'tsoa';
+
+import { User } from './user';
+import { UserCreationParams, UsersService } from './usersService';
+
+interface ValidateErrorJSON {
+  message: "Validation failed";
+  details: { [name: string]: unknown };
+}
 
 @Route("users")
 export class UsersController extends Controller {
+  @Response<ValidateErrorJSON>(404, "Not Found")
   @Get("{userId}")
   public async getUser(
     @Path() userId: number,
+    @Res() notFoundResponse: TsoaResponse<404, { reason: string }>,
     @Query() name?: string
   ): Promise<User> {
-    return new UsersService().get(userId, name);
+    const fetchedUser = new UsersService().get(userId, name);
+
+    if (!fetchedUser) {
+      return notFoundResponse(404, { reason: "User not found" });
+    }
+
+    return fetchedUser;
   }
 
+  @Response<ValidateErrorJSON>(422, "Validation Failed")
   @SuccessResponse("201", "Created") // Custom success response
   @Post()
   public async createUser(
